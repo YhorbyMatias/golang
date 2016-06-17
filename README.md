593e0db387a1b05535afe2344860c41dc2b10115
yhorby matias <yheredia@yhorby.com>
2016-06-17 03:32:02 -0700

    first

---
+++ README.md
@@ -0,0 +1,1 @@
+# Example how for works on Golang
---
+++ for.go
@@ -0,0 +1,24 @@
+  // ejemplo uso  for edu
+
+package main
+
+import "fmt"
+
+
+func main () {
+
+  alumnos :=1
+fmt.Println("Alumnos")
+   for alumnos <= 3 {
+fmt.Println(alumnos)
+alumnos =  alumnos + 1
+}
+fmt.Println("Calificaciones")
+for calificaciones :=7; calificaciones <=9; {
+fmt.Println(calificaciones)
+calificaciones = calificaciones +1
+}
+}
+
+
+
