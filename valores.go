9c5156b6c916797627627550423fe1dbdef22bdd
yhorby matias <yheredia@yhorby.com>
2016-07-01 07:55:14 -0700

    adding valores.go

---
+++ valores.go
@@ -0,0 +1,24 @@
+//this code show how works with values
+package main
+import "fmt"
+func main() {
+     // concatenate strings
+   fmt.Println("Yhor" + "by")
+// how to sum or aritmetics operations
+ fmt.Println("5-5 =", 5-5)
+ fmt.Println("5+5 =", 5+5)
+ fmt.Println("5*5 =", 5*5)
+ fmt.Println("5.30/5 =", 5.30/5)
+
+//valores booleanos
+ fmt.Println(true && false)
+    fmt.Println(true || false)
+    fmt.Println(!true)
+
+
+
+
+
+}
+
+
